76c76a6c122f1e43acebb7a6711abfdf8d764d2c
wiranoid <wiranoid@gmail.com>
2015-02-25 22:22:22 +0300

    Add uglify-task in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
     livereload = require('gulp-livereload'),
     plumber = require('gulp-plumber'),
     minifyCSS = require('gulp-minify-css');
+
+
+gulp.task('js', function() {
+    gulp.src('./js/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/js'));
+});
+
+
+gulp.task('js-libs', function() {
+    gulp.src('./public/libs/**/*.js')
+        .pipe(gulp.dest('./build/public/libs'));
+});
 
 
 gulp.task('styles', function() {
@@ -17,10 +31,9 @@ gulp.task('styles', function() {
 
 gulp.task('watch', function() {
     livereload.listen();
+    gulp.watch('./js/**/*.js', ['js']);
     gulp.watch('./sass/*.scss', ['styles']);
 });
 
 
-gulp.task('default', function() {
-    console.log('Hello gulp!');
-});
+gulp.task('default', ['js-libs', 'js', 'styles', 'watch']);
--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 
 require.config({
     paths: {
-        jquery: '../public/libs/jquery/dist/jquery.min',
-        underscore: '../public/libs/underscore/underscore-min',
+        jquery: '../public/libs/jquery/dist/jquery',
+        underscore: '../public/libs/underscore/underscore',
         backbone: '../public/libs/backbone/backbone'
     }
 });
 
 require([
-	// Load our app module and pass it to our definition function
+    // Load our app module and pass it to our definition function
     'app',
     ], function(app) {
     // The "app" dependency is passed in as "app"
